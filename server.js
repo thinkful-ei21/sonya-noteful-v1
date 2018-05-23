034248e28607f35c00a9a2b6ece51b475ff6b544
Sonya Slegers <sonjay08@gmail.com>
2018-05-23 16:20:07 -0500

    update client with post and delete note functions

--- server.js
+++ server.js
@@ -3,82 +3,26 @@
 // Load array of notes
 const express = require('express');
 
-
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
-
 const morgan = require('morgan');
-
-
 
 const {PORT} = require('./config');
 
 // Create an Express application
 const app = express();
 
-// log all requests
-app.use(morgan('dev'));
-
-// create a static webserver
-app.use(express.static('public'));
+const notesRouter = require('./router/notes.router.js');
 
 // parse request body
 app.use(express.json());
 
-// Get All (and search by query)
-app.get('/api/notes', (req, res, next) => {
-  const {searchTerm} = req.query;
 
-  notes.filter(searchTerm, (err, list) => {
-    if(err) {
-      return next(err);
-    }
-    res.json(list);
-  });
-});
+// logs the http layer
+app.use(morgan('dev'));
 
-// Get a single item
-app.get('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
+app.use(express.static('public'));
 
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  /******Never trust users - validate input*******/
-
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
+//mount routers
+app.use('/api', notesRouter);
 
 //catch-all 404
 app.use(function (req, res, next) {
